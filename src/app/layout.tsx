56079d2e9808e3f8364c243d35dd8182f1a9d524
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-05-28 04:20:07 +0100

    dashboard layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
 import type { Metadata } from "next";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider, SignInButton, SignedOut } from "@clerk/nextjs";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -25,9 +19,6 @@ export default function RootLayout({
           <SignedOut>
             <SignInButton />
           </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
           {children}
         </body>
       </html>
